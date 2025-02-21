d1cffd71b0130c85960701ead3b626d2821c2fb3
jhoffman-ms <jmh248@gmail.com>
2025-02-21 16:10:14 -0300

    feat: update logic to calculate the addon surplus when is cm user

---
+++ DopplerBillingJob/Entities/SurplusAddOn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Doppler.Billing.Job.Entities
+{
+    public class SurplusAddOn
+    {
+        public int IdUser { get; set; }
+        public int IdAddOnType { get; set; }
+        public DateTime Date { get; set; }
+        public string Period { get; set; }
+        public int Quantity { get; set; }
+        public decimal AdditionalPrice { get; set; }
+        public decimal Total { get; set; }
+
+    }
+}
