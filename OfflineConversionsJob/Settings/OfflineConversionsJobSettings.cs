2c14eea01be3600345829e50a72dd3474dcec23d
Arian Provenzano <aprovenzano@ms.local>
2025-11-18 16:21:42 -0300

    feat: Create Offline Conversion Job

---
+++ OfflineConversionsJob/Settings/OfflineConversionsJobSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace OfflineConversionsJob.Settings;
+
+public class OfflineConversionsJobSettings
+{
+    public string GoogleClientCustomerId { get; set; }
+    public string GoogleConversionActionId { get; set;  }
+    public string GoogleDeveloperToken { get; set; }
+    public string GoogleLoginCustomerId { get; set; }
+    public string GoogleOAuthClientId { get; set; }
+    public string GoogleOAuthClientSecret { get; set; }
+    public string GoogleAuthRefreshToken { get; set; }
+    public string ZohoClientId { get; set; }
+    public string ZohoClientSecret { get; set; }
+    public string ZohoRefreshToken { get; set; }
+    
+}
